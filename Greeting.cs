672fbac09f52bed5c8a6049f0c0f76f836a08988
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-23 12:04:57 +0530

    hello world in c#

---
+++ Greeting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Greeting
+    {
+        public static void greet(string name)
+        {
+            Console.WriteLine($"Hello World from {name} !!");
+        }
+    }
+}
