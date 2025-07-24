3512c051e0eced5a6e644f7ecfd68401533e1b80
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-24 12:14:28 +0530

    add ConsoleClassPractice

---
+++ ConsoleClassPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Title = "Console Class Practice";
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Clear();
+    }
+}
