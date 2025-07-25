44e4046e955db0ba71dd181fd5248cf3bffb7f97
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-25 13:45:44 +0530

    adding const vs readonly practice program files

---
+++ Const and ReadOnly/Program.cs
@@ -0,0 +1,31 @@
+﻿class ConstantsExample
+{
+    // const: compile-time constant
+    public const double Pi = 3.14159;
+
+    // readonly: runtime constant
+    public readonly int Id;
+
+    public ConstantsExample(int id)
+    {
+        Id = id; // Readonly can be initialzed in constructor
+    }
+
+    public void ShowDetails()
+    {
+        Console.WriteLine($"Pi: {Pi}");
+        Console.WriteLine($"ID: {Id}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConstantsExample obj1 = new ConstantsExample(101);
+        ConstantsExample obj2 = new ConstantsExample(202);
+
+        obj1.ShowDetails();
+        obj2.ShowDetails();
+    }
+}
