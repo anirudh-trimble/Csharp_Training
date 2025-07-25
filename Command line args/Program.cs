c2dab14e4860c3e882a3b78c9ff4a2571cea3ed5
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-25 11:47:23 +0530

    added command line args practice file

---
+++ Command line args/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("No arguments were passed.");
+        }
+        else
+        {
+            Console.WriteLine($"Number of arguments: {args.Length}");
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Argument {i + 1}: {args[i]}");
+            }
+
+            // convert first 2 arguments to integers and add them
+            if (args.Length >= 2)
+            {
+                if (int.TryParse(args[0], out int num1) && int.TryParse(args[1], out int num2))
+                {
+                    Console.WriteLine($"Sum: {num1 + num2}");
+                }
+                else
+                {
+                    Console.WriteLine("To calculate sum, enter first two arguments as numbers.");
+                }
+            }
+        }
+    }
+}
