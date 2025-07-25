eac64c42c4274b1b8956f5c505d4040cc773a657
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-25 12:13:27 +0530

    added boxing and unboxing program practice files

---
+++ Boxing unboxing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 42;
+
+        // Boxing
+        object boxedNumber = number;
+
+        Console.WriteLine("Boxed value: " + boxedNumber);
+
+        // Unboxing
+        int unboxedNumber = (int)boxedNumber;
+
+        Console.WriteLine("Unboxed value: " + unboxedNumber);
+
+        // Changing original value to show they're independent
+        number = 100;
+        Console.WriteLine("Original value after change: " + number);
+        Console.WriteLine("Boxed value remains: " + boxedNumber);
+    }
+}
