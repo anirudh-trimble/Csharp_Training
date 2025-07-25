61f856565773994ead7db09acf633e9f9aa1db47
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-25 15:17:07 +0530

    adding properties example program file

---
+++ Properties in Csharp/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Student
+{
+    private int rollNo;
+
+    // Read-Write Property
+    public int RollNo
+    {
+        get
+        {
+            return rollNo;
+        }
+        set
+        {
+            rollNo = value;
+        }
+    }
+
+    public Student(int rollNo)
+    {
+        this.RollNo = rollNo;
+    }
+}
+
+public class Geeks
+{
+    public static void Main(string[] args)
+    {
+        Student obj = new Student(147);
+        Console.WriteLine($"Roll No: {obj.RollNo}");
+
+        obj.RollNo = 123;
+        Console.WriteLine($"Changed Roll No: {obj.RollNo}");
+    }
+}
