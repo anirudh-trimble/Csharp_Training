79a5065c27fcaba1092444a4267f881d63cf40d4
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-28 14:52:49 +0530

    adding garbage collection practice programs

---
+++ GarbageCollection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+class MyClass
+{
+    public MyClass()
+    {
+        Console.WriteLine("MyClass object created");
+    }
+
+    ~MyClass()
+    {
+        Console.WriteLine("MyClass object destroyed");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass obj = new MyClass();
+        obj = null;
+
+        // Force garbage collection
+        GC.Collect();
+        GC.WaitForPendingFinalizers(); // Make sure all finalizers have run
+
+        Console.WriteLine("End of Main");
+    }
+}
