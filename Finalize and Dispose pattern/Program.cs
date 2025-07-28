f8befae9bb0220c65faf56797df92ad5cab6d5fb
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-28 15:05:10 +0530

    finalize and dispose pattern code

---
+++ Finalize and Dispose pattern/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class MyResource : IDisposable
+{
+    private bool disposed = false;
+
+    // Simulate unmanaged resource
+    private IntPtr unmanagedResource;
+
+    public MyResource()
+    {
+        Console.WriteLine("Resource acquired.");
+        // Simulate allocation of unmanaged memory
+        unmanagedResource = new IntPtr(123);
+    }
+
+    // Finalizer (destructor)
+    ~MyResource()
+    {
+        Dispose(false);
+    }
+
+    // Public Dispose method
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this); // Prevent finalizer from running
+    }
+
+    // Core dispose logic
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                // Free managed resources here
+                Console.WriteLine("Releasing managed resources...");
+            }
+
+            // Free unmanaged resources here
+            Console.WriteLine("Releasing unmanaged resources...");
+            unmanagedResource = IntPtr.Zero;
+
+            disposed = true;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        using (MyResource res = new MyResource())
+        {
+            // Use the resource
+        } // Dispose() is called automatically here
+
+        Console.WriteLine("End of Main");
+    }
+}
+
