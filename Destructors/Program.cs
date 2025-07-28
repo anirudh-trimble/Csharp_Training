1f81818c099230f693782a67fda9f50feb9b5e99
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-28 14:42:24 +0530

    pushing simple destructors example program file

---
+++ Destructors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class MyClass
+{
+    public MyClass()
+    {
+        Console.WriteLine("Constructor: MyClass object created.");
+    }
+
+    public void SayHello()
+    {
+        Console.WriteLine("Hello from MyClass!");
+    }
+
+    // Destructor
+    ~MyClass()
+    {
+        Console.WriteLine("Destructor: MyClass object is being destroyed.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass obj = new MyClass();
+        obj.SayHello();
+
+        // Forcing garbage collection for demo
+        obj = null;
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+
+        Console.WriteLine("End of Main method.");
+    }
+}
