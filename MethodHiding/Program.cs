0326f8e0042dabc9eaf57ce6e68268aa1e2718f8
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-29 11:04:08 +0530

    pushing method hiding practice file

---
+++ MethodHiding/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Animal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Animal speaks");
+    }
+}
+
+class Dog : Animal
+{
+    public new void Speak()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Dog d = new Dog();
+        d.Speak();  // Output: Dog barks
+
+        Animal a = new Dog();
+        a.Speak();  // Output: Animal speaks (method hiding!)
+    }
+}
