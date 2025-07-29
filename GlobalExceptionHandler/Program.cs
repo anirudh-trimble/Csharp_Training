18560363d987fff22496d049c7d7c19a76884440
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-29 15:04:50 +0530

    pushing global exception handling simple program

---
+++ GlobalExceptionHandler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Register global exception handler
+        AppDomain.CurrentDomain.UnhandledException += GlobalExceptionHandler;
+
+        Console.WriteLine("Application started.");
+
+        // Deliberately throw an unhandled exception
+        ThrowException();
+
+        Console.WriteLine("This line will not be executed.");
+    }
+
+    static void ThrowException()
+    {
+        throw new Exception("Something went wrong!");
+    }
+
+    static void GlobalExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+    {
+        Console.WriteLine("Global Exception Handler caught: " + ((Exception)e.ExceptionObject).Message);
+    }
+}
