f1a2825337d4311fbdbb5987b7e26ede0d00c468
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-29 14:57:46 +0530

    added throw ex program file

---
+++ Throw ex/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            CauseException();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Caught in Main:");
+            Console.WriteLine(ex.StackTrace);
+        }
+    }
+
+    static void CauseException()
+    {
+        try
+        {
+            int x = 0;
+            int y = 10 / x; // Divide by zero
+        }
+        catch (Exception ex)
+        {
+            // throw;      // preserves original stack trace
+            throw ex;     // resets stack trace to this line
+        }
+    }
+}
