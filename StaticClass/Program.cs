ed72262fdd7a77f26b4b92bf1c5bf90cda7be949
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-29 12:10:51 +0530

    static class practice program added

---
+++ StaticClass/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StaticClassDemo
+{
+    // Static class
+    public static class MathUtility
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double SquareRoot(double x)
+        {
+            return Math.Sqrt(x);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = MathUtility.Add(10, 5);
+            int product = MathUtility.Multiply(4, 3);
+            double root = MathUtility.SquareRoot(49);
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Product: {product}");
+            Console.WriteLine($"Square Root: {root}");
+        }
+    }
+}
