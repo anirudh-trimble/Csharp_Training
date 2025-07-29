2f37d054bc9cb0011370afdfc4b2952c0f79989d
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-29 10:18:42 +0530

    adding multiple inheritace using interface practice program

---
+++ MultipleInheritance using interfaces/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+interface IPrinter
+{
+    void Print();
+}
+
+interface IScanner
+{
+    void Scan();
+}
+
+// Implements both interfaces
+class MultiFunctionMachine : IPrinter, IScanner
+{
+    public void Print()
+    {
+        Console.WriteLine("Printing document...");
+    }
+
+    public void Scan()
+    {
+        Console.WriteLine("Scanning document...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MultiFunctionMachine mfm = new MultiFunctionMachine();
+        mfm.Print();  // Output: Printing document...
+        mfm.Scan();   // Output: Scanning document...
+    }
+}
