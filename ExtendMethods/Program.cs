d83f039699e2898599a58cd390e5221924b0cc71
EU\aniruds <anirudhkrishna_s@trimble.com>
2025-07-29 11:46:07 +0530

    adding extension methods example program

---
+++ ExtendMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class StringExtensions
+{
+    public static int WordCount(this string str)
+    {
+        return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string message = "Hello from the other side.";
+        Console.WriteLine(message.WordCount());  // Output: 5
+    }
+}
